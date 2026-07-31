122db070669763d3871dd51a0f7a0b855ab4910e
Akash007sharma16 <Akash007sharma16@gmail.com>
2026-07-31 17:23:05 +0530

    Add Array2D and DynamicArray projects

---
+++ Array2D/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static List<int> ReverseArray(List<int> numbers)
+    {
+        List<int> reversedArray = new List<int>();
+
+        for (int i = numbers.Count - 1; i >= 0; i--)
+        {
+            reversedArray.Add(numbers[i]);
+        }
+
+        return reversedArray;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the number of elements: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the array elements separated by space: ");
+        List<int> numbers = Console.ReadLine()
+                                   .Split(' ')
+                                   .Select(int.Parse)
+                                   .ToList();
+
+        List<int> result = ReverseArray(numbers);
+
+        Console.WriteLine("\nReversed Array:");
+
+        foreach (int number in result)
+        {
+            Console.Write(number + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
---
+++ DynamicArray/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static List<int> DynamicArray(int n, List<List<int>> queries)
+    {
+        List<List<int>> arr = new List<List<int>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            arr.Add(new List<int>());
+        }
+
+        List<int> result = new List<int>();
+        int lastAnswer = 0;
+
+        foreach (List<int> query in queries)
+        {
+            int type = query[0];
+            int x = query[1];
+            int y = query[2];
+
+            int index = (x ^ lastAnswer) % n;
+
+            if (type == 1)
+            {
+                arr[index].Add(y);
+            }
+            else if (type == 2)
+            {
+                lastAnswer = arr[index][y % arr[index].Count];
+                result.Add(lastAnswer);
+            }
+        }
+
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Enter n and q: ");
+        string[] input = Console.ReadLine().Split(' ');
+
+        int n = int.Parse(input[0]);
+        int q = int.Parse(input[1]);
+
+        List<List<int>> queries = new List<List<int>>();
+
+        Console.WriteLine("Enter the queries:");
+
+        for (int i = 0; i < q; i++)
+        {
+            string[] queryInput = Console.ReadLine().Split(' ');
+
+            List<int> query = new List<int>();
+
+            foreach (string value in queryInput)
+            {
+                query.Add(int.Parse(value));
+            }
+
+            queries.Add(query);
+        }
+
+        List<int> answer = DynamicArray(n, queries);
+
+        Console.WriteLine("\nOutput:");
+
+        foreach (int value in answer)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
