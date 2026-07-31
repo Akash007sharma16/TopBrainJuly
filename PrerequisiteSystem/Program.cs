7e293f8f9c80479c4c10319ceed6392d6d98a2c4
Akash007sharma16 <Akash007sharma16@gmail.com>
2026-07-31 14:36:28 +0530

    Initial commit: Add PrerequisiteSystem and SocialNetworkRecommendation projects

---
+++ PrerequisiteSystem/Program.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+
+class CourseGraph
+{
+    private readonly List<int>[] graph;
+    private readonly int totalCourses;
+
+    public CourseGraph(int numberOfCourses)
+    {
+        totalCourses = numberOfCourses;
+        graph = new List<int>[numberOfCourses];
+
+        for (int i = 0; i < numberOfCourses; i++)
+        {
+            graph[i] = new List<int>();
+        }
+    }
+
+    // Adds a prerequisite relationship
+    public void AddPrerequisite(int prerequisite, int course)
+    {
+        graph[prerequisite].Add(course);
+    }
+
+    // Displays the graph
+    public void DisplayGraph()
+    {
+        Console.WriteLine("Course Dependency Graph\n");
+
+        for (int i = 0; i < totalCourses; i++)
+        {
+            Console.Write("Course " + i + " -> ");
+
+            foreach (int course in graph[i])
+            {
+                Console.Write(course + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    // Finds all prerequisites for a given course
+    public void ShowAllPrerequisites(int course)
+    {
+        bool[] visited = new bool[totalCourses];
+
+        Console.WriteLine("\nAll prerequisites for Course " + course + ":");
+
+        FindPrerequisites(course, visited);
+    }
+
+    private void FindPrerequisites(int course, bool[] visited)
+    {
+        for (int i = 0; i < totalCourses; i++)
+        {
+            if (graph[i].Contains(course) && !visited[i])
+            {
+                visited[i] = true;
+                Console.WriteLine("Course " + i);
+
+                FindPrerequisites(i, visited);
+            }
+        }
+    }
+
+    // Shows only direct prerequisites
+    public void ShowDirectPrerequisites(int course)
+    {
+        Console.WriteLine("\nDirect prerequisites for Course " + course + ":");
+
+        bool found = false;
+
+        for (int i = 0; i < totalCourses; i++)
+        {
+            if (graph[i].Contains(course))
+            {
+                Console.WriteLine("Course " + i);
+                found = true;
+            }
+        }
+
+        if (!found)
+        {
+            Console.WriteLine("No direct prerequisites.");
+        }
+    }
+
+    // Cycle detection
+    public bool ContainsCycle()
+    {
+        bool[] visited = new bool[totalCourses];
+        bool[] recursionStack = new bool[totalCourses];
+
+        for (int i = 0; i < totalCourses; i++)
+        {
+            if (IsCyclePresent(i, visited, recursionStack))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private bool IsCyclePresent(int currentCourse, bool[] visited, bool[] recursionStack)
+    {
+        if (recursionStack[currentCourse])
+        {
+            return true;
+        }
+
+        if (visited[currentCourse])
+        {
+            return false;
+        }
+
+        visited[currentCourse] = true;
+        recursionStack[currentCourse] = true;
+
+        foreach (int nextCourse in graph[currentCourse])
+        {
+            if (IsCyclePresent(nextCourse, visited, recursionStack))
+            {
+                return true;
+            }
+        }
+
+        recursionStack[currentCourse] = false;
+
+        return false;
+    }
+
+    // Topological Sort
+    public void DisplayCourseOrder()
+    {
+        int[] inDegree = new int[totalCourses];
+
+        for (int i = 0; i < totalCourses; i++)
+        {
+            foreach (int course in graph[i])
+            {
+                inDegree[course]++;
+            }
+        }
+
+        Queue<int> readyCourses = new Queue<int>();
+
+        for (int i = 0; i < totalCourses; i++)
+        {
+            if (inDegree[i] == 0)
+            {
+                readyCourses.Enqueue(i);
+            }
+        }
+
+        Console.WriteLine("\nRecommended Course Order:");
+
+        while (readyCourses.Count > 0)
+        {
+            int currentCourse = readyCourses.Dequeue();
+
+            Console.Write(currentCourse + " ");
+
+            foreach (int nextCourse in graph[currentCourse])
+            {
+                inDegree[nextCourse]--;
+
+                if (inDegree[nextCourse] == 0)
+                {
+                    readyCourses.Enqueue(nextCourse);
+                }
+            }
+        }
+
+        Console.WriteLine();
+    }
+
+    // Courses that can be taken first
+    public void DisplayCoursesWithoutPrerequisites()
+    {
+        int[] inDegree = new int[totalCourses];
+
+        for (int i = 0; i < totalCourses; i++)
+        {
+            foreach (int course in graph[i])
+            {
+                inDegree[course]++;
+            }
+        }
+
+        Console.WriteLine("\nCourses with no prerequisites:");
+
+        for (int i = 0; i < totalCourses; i++)
+        {
+            if (inDegree[i] == 0)
+            {
+                Console.WriteLine("Course " + i);
+            }
+        }
+    }
+
+    // Count direct dependent courses
+    public void CountDependentCourses(int course)
+    {
+        Console.WriteLine("\nCourses directly depending on Course " + course + ": " + graph[course].Count);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        CourseGraph courseGraph = new CourseGraph(6);
+
+        // Prerequisite relationships
+        courseGraph.AddPrerequisite(0, 1);
+        courseGraph.AddPrerequisite(0, 2);
+        courseGraph.AddPrerequisite(1, 3);
+        courseGraph.AddPrerequisite(2, 3);
+        courseGraph.AddPrerequisite(2, 4);
+        courseGraph.AddPrerequisite(3, 5);
+        courseGraph.AddPrerequisite(4, 5);
+
+        courseGraph.DisplayGraph();
+
+        courseGraph.ShowAllPrerequisites(5);
+
+        courseGraph.ShowDirectPrerequisites(3);
+
+        Console.WriteLine("\nCycle Found : " + courseGraph.ContainsCycle());
+
+        if (!courseGraph.ContainsCycle())
+        {
+            courseGraph.DisplayCourseOrder();
+        }
+
+        courseGraph.DisplayCoursesWithoutPrerequisites();
+
+        courseGraph.CountDependentCourses(2);
+
+        Console.ReadKey();
+    }
+}
---
+++ SocialNetworkRecommendation/Program.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+
+class SocialNetwork
+{
+    private readonly List<int>[] friends;
+    private readonly int totalUsers;
+
+    public SocialNetwork(int numberOfUsers)
+    {
+        totalUsers = numberOfUsers;
+        friends = new List<int>[numberOfUsers];
+
+        for (int i = 0; i < numberOfUsers; i++)
+        {
+            friends[i] = new List<int>();
+        }
+    }
+
+    public void AddFriendship(int user1, int user2)
+    {
+        friends[user1].Add(user2);
+        friends[user2].Add(user1);
+    }
+
+    public void DisplayFriends(int user)
+    {
+        Console.Write("Friends of User " + user + ": ");
+
+        foreach (int friend in friends[user])
+        {
+            Console.Write(friend + " ");
+        }
+
+        Console.WriteLine();
+    }
+
+    public bool AreConnected(int startUser, int endUser)
+    {
+        bool[] visited = new bool[totalUsers];
+
+        return SearchConnection(startUser, endUser, visited);
+    }
+
+    private bool SearchConnection(int currentUser, int targetUser, bool[] visited)
+    {
+        if (currentUser == targetUser)
+        {
+            return true;
+        }
+
+        visited[currentUser] = true;
+
+        foreach (int friend in friends[currentUser])
+        {
+            if (!visited[friend])
+            {
+                if (SearchConnection(friend, targetUser, visited))
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public void FindShortestPath(int startUser, int endUser)
+    {
+        bool[] visited = new bool[totalUsers];
+        int[] parent = new int[totalUsers];
+
+        for (int i = 0; i < totalUsers; i++)
+        {
+            parent[i] = -1;
+        }
+
+        Queue<int> queue = new Queue<int>();
+
+        queue.Enqueue(startUser);
+        visited[startUser] = true;
+
+        while (queue.Count > 0)
+        {
+            int current = queue.Dequeue();
+
+            foreach (int friend in friends[current])
+            {
+                if (!visited[friend])
+                {
+                    visited[friend] = true;
+                    parent[friend] = current;
+                    queue.Enqueue(friend);
+                }
+            }
+        }
+
+        if (!visited[endUser])
+        {
+            Console.WriteLine("\nNo path exists.");
+            return;
+        }
+
+        Stack<int> path = new Stack<int>();
+
+        int temp = endUser;
+
+        while (temp != -1)
+        {
+            path.Push(temp);
+            temp = parent[temp];
+        }
+
+        Console.Write("\nShortest Path: ");
+
+        while (path.Count > 0)
+        {
+            Console.Write(path.Pop());
+
+            if (path.Count > 0)
+            {
+                Console.Write(" -> ");
+            }
+        }
+
+        Console.WriteLine();
+    }
+
+    public void UsersAtDistanceTwo(int user)
+    {
+        bool[] visited = new bool[totalUsers];
+        Queue<(int user, int distance)> queue = new Queue<(int, int)>();
+
+        visited[user] = true;
+        queue.Enqueue((user, 0));
+
+        Console.Write("\nUsers at distance 2 from User " + user + ": ");
+
+        while (queue.Count > 0)
+        {
+            var current = queue.Dequeue();
+
+            if (current.distance == 2)
+            {
+                Console.Write(current.user + " ");
+                continue;
+            }
+
+            foreach (int friend in friends[current.user])
+            {
+                if (!visited[friend])
+                {
+                    visited[friend] = true;
+                    queue.Enqueue((friend, current.distance + 1));
+                }
+            }
+        }
+
+        Console.WriteLine();
+    }
+
+    public bool HasCycle()
+    {
+        bool[] visited = new bool[totalUsers];
+
+        for (int i = 0; i < totalUsers; i++)
+        {
+            if (!visited[i])
+            {
+                if (DetectCycle(i, visited, -1))
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private bool DetectCycle(int current, bool[] visited, int parent)
+    {
+        visited[current] = true;
+
+        foreach (int friend in friends[current])
+        {
+            if (!visited[friend])
+            {
+                if (DetectCycle(friend, visited, current))
+                {
+                    return true;
+                }
+            }
+            else if (friend != parent)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void DisplayFriendGroups()
+    {
+        bool[] visited = new bool[totalUsers];
+
+        Console.WriteLine("\nConnected Components:");
+
+        for (int i = 0; i < totalUsers; i++)
+        {
+            if (!visited[i])
+            {
+                PrintComponent(i, visited);
+                Console.WriteLine();
+            }
+        }
+    }
+
+    private void PrintComponent(int user, bool[] visited)
+    {
+        visited[user] = true;
+
+        Console.Write(user + " ");
+
+        foreach (int friend in friends[user])
+        {
+            if (!visited[friend])
+            {
+                PrintComponent(friend, visited);
+            }
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        SocialNetwork network = new SocialNetwork(6);
+
+        network.AddFriendship(0, 1);
+        network.AddFriendship(0, 2);
+        network.AddFriendship(1, 3);
+        network.AddFriendship(2, 3);
+        network.AddFriendship(2, 4);
+        network.AddFriendship(3, 5);
+        network.AddFriendship(4, 5);
+
+        network.DisplayFriends(2);
+
+        Console.WriteLine("\nUser 0 and User 5 Connected : " +
+                          network.AreConnected(0, 5));
+
+        network.FindShortestPath(0, 5);
+
+        network.UsersAtDistanceTwo(1);
+
+        Console.WriteLine("\nCycle Present : " + network.HasCycle());
+
+        network.DisplayFriendGroups();
+
+        Console.ReadKey();
+    }
+}
