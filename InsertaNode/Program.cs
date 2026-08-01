40c77a7bf96d831c1be94bffac9c51c6e14a19aa
Akash007sharma16 <Akash007sharma16@gmail.com>
2026-08-01 11:58:03 +0530

    Add InsertaNode and PostOrderTraversal projects

---
+++ InsertaNode/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+class Node
+{
+    public int data;
+    public Node next;
+
+    public Node(int data)
+    {
+        this.data = data;
+        next = null;
+    }
+}
+
+class LinkedList
+{
+    public Node head;
+    public Node tail;
+
+    public void InsertNode(int data)
+    {
+        Node node = new Node(data);
+
+        if (head == null)
+        {
+            head = node;
+            tail = node;
+        }
+        else
+        {
+            tail.next = node;
+            tail = node;
+        }
+    }
+
+    public void PrintList()
+    {
+        Node current = head;
+
+        while (current != null)
+        {
+            Console.Write(current.data + " ");
+            current = current.next;
+        }
+
+        Console.WriteLine();
+    }
+
+    public void InsertAtPosition(int data, int position)
+    {
+        Node newNode = new Node(data);
+
+        // Insert at the beginning
+        if (position == 0)
+        {
+            newNode.next = head;
+            head = newNode;
+
+            if (tail == null)
+                tail = newNode;
+
+            return;
+        }
+
+        Node current = head;
+
+        // Move to the node before the insertion position
+        for (int i = 0; i < position - 1; i++)
+        {
+            current = current.next;
+        }
+
+        newNode.next = current.next;
+        current.next = newNode;
+
+        // Update tail if inserted at the end
+        if (newNode.next == null)
+            tail = newNode;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        LinkedList list = new LinkedList();
+
+        Console.Write("Enter number of nodes: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the node values:");
+
+        for (int i = 0; i < n; i++)
+        {
+            int value = int.Parse(Console.ReadLine());
+            list.InsertNode(value);
+        }
+
+        Console.Write("Enter data to insert: ");
+        int data = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter position: ");
+        int position = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("\nOriginal List:");
+        list.PrintList();
+
+        list.InsertAtPosition(data, position);
+
+        Console.WriteLine("List After Insertion:");
+        list.PrintList();
+    }
+}
---
+++ PostOrderTraversal/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+class Node
+{
+    public int data;
+    public Node left, right;
+
+    public Node(int data)
+    {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Program
+{
+    // Insert a node into the Binary Search Tree
+    static Node Insert(Node root, int data)
+    {
+        if (root == null)
+            return new Node(data);
+
+        if (data <= root.data)
+            root.left = Insert(root.left, data);
+        else
+            root.right = Insert(root.right, data);
+
+        return root;
+    }
+
+    // Postorder Traversal (Left -> Right -> Root)
+    static void PostOrder(Node root)
+    {
+        if (root == null)
+            return;
+
+        PostOrder(root.left);
+        PostOrder(root.right);
+        Console.Write(root.data + " ");
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Enter number of nodes: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter node values:");
+
+        string[] input = Console.ReadLine().Split();
+
+        Node root = null;
+
+        for (int i = 0; i < n; i++)
+        {
+            root = Insert(root, int.Parse(input[i]));
+        }
+
+        Console.Write("\nPostorder Traversal: ");
+        PostOrder(root);
+        Console.WriteLine();
+    }
+}
