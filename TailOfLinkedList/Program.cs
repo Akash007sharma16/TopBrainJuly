6f47d50a48ad72d5c2e7ae68fba3de2349025ac5
Akash007sharma16 <Akash007sharma16@gmail.com>
2026-08-01 12:32:07 +0530

    Add TailOfLinkedList and TruckTour projects

---
+++ TailOfLinkedList/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+class Node
+{
+    public int data;
+    public Node next;
+
+    public Node(int data)
+    {
+        this.data = data;
+        next = null;
+    }
+}
+
+class LinkedList
+{
+    public Node head;
+    public Node tail;
+
+    // Insert at Tail
+    public void InsertAtTail(int data)
+    {
+        Node newNode = new Node(data);
+
+        if (head == null)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    // Display the Linked List
+    public void Display()
+    {
+        Node current = head;
+
+        while (current != null)
+        {
+            Console.Write(current.data + " ");
+            current = current.next;
+        }
+
+        Console.WriteLine();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        LinkedList list = new LinkedList();
+
+        Console.Write("Enter the number of nodes: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the node values:");
+
+        for (int i = 0; i < n; i++)
+        {
+            int value = int.Parse(Console.ReadLine());
+            list.InsertAtTail(value);
+        }
+
+        Console.WriteLine("\nLinked List:");
+
+        list.Display();
+    }
+}
---
+++ TruckTour/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static int TruckTour(List<List<int>> petrolPumps)
+    {
+        int start = 0;
+        long balance = 0;
+        long deficit = 0;
+
+        for (int i = 0; i < petrolPumps.Count; i++)
+        {
+            balance += petrolPumps[i][0] - petrolPumps[i][1];
+
+            if (balance < 0)
+            {
+                start = i + 1;
+                deficit += balance;
+                balance = 0;
+            }
+        }
+
+        if (balance + deficit >= 0)
+            return start;
+        else
+            return -1;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Enter number of petrol pumps: ");
+        int n = int.Parse(Console.ReadLine());
+
+        List<List<int>> petrolPumps = new List<List<int>>();
+
+        Console.WriteLine("Enter petrol and distance for each pump:");
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            int petrol = int.Parse(input[0]);
+            int distance = int.Parse(input[1]);
+
+            petrolPumps.Add(new List<int> { petrol, distance });
+        }
+
+        int result = TruckTour(petrolPumps);
+
+        Console.WriteLine("\nStarting Petrol Pump Index: " + result);
+    }
+}
